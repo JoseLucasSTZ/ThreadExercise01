b9db5e8fc6505b2335a079b0c85b57cddcc75b8a
José Lucas Steinmetz da Silveira <65045856+joselucasSteinmetz@users.noreply.github.com>
2023-03-21 21:23:25 -0300

    Add files via upload

---
+++ src/ThreadPrincipal.java
@@ -0,0 +1,10 @@
+
+public class ThreadPrincipal {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Numero de threads:" + Thread.activeCount());
+        System.out.println(Thread.currentThread());
+    }
+    
+}
